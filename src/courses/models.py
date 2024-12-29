590292386cb3d184c9ed1bb3f021d6f4f583c0bb
Pablo Rioseco <pablo.riosecov@gmail.com>
2024-12-29 19:13:11 -0300

    Cloudinary video and thumbnail fields

--- src/courses/models.py
+++ src/courses/models.py
@@ -73,6 +73,8 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
+    thumbnail = CloudinaryField('image', blank=True, null=True)
+    video = CloudinaryField('video', blank=True, null=True, resource_type="video")
     can_preview = models.BooleanField(default=False, help_text="If the user does not have access to the course, they can preview this lesson.")
     status = models.CharField(
         max_length=10, 
