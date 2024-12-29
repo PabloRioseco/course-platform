c1bf9ccc393ab729300dc2610d9b2d774450a92e
Pablo Rioseco <pablo.riosecov@gmail.com>
2024-12-29 19:07:41 -0300

    Lesson publish status & preview

--- src/courses/models.py
+++ src/courses/models.py
@@ -35,3 +35,47 @@ class Course(models.Model):
     @property
     def is_published(self):
         return self.status == PublishStatus.PUBLISHED
+    
+    @property
+    def image_admin(self):
+        if not self.image:
+            return ""
+        image_options = {
+            "width": 200
+        }
+        url = self.image.build_url(**image_options)
+        return url
+    
+    def get_image_thumbnail(self, as_html=False, width=500):
+        if not self.image:
+            return ""
+        image_options = {
+            "width": width
+        }
+        if as_html:
+            return self.image.image(**image_options)
+        url = self.image.build_url(**image_options)
+        return url
+
+    def get_image_detail(self, as_html=False, width=750):
+        if not self.image:
+            return ""
+        image_options = {
+            "width": width
+        }
+        if as_html:
+            return self.image.image(**image_options)
+        url = self.image.build_url(**image_options)
+        return url
+    
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    can_preview = models.BooleanField(default=False, help_text="If the user does not have access to the course, they can preview this lesson.")
+    status = models.CharField(
+        max_length=10, 
+        choices=PublishStatus.choices,
+        default=PublishStatus.PUBLISHED
+    )
