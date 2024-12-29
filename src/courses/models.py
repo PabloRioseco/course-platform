7d84b2b3d26ccb5b1b4271c4797eba60736c01c4
Pablo Rioseco <pablo.riosecov@gmail.com>
2024-12-29 20:04:12 -0300

    CloudinaryField Upload Options

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,5 +1,7 @@
+import uuid
 import helpers
 from django.db import models
+from django.utils.text import slugify
 from cloudinary.models import CloudinaryField
 
 helpers.cloudinary_init()
@@ -16,11 +18,33 @@ class PublishStatus(models.TextChoices):
 def handle_upload(instance, filename):
     return f"{filename}"
 
+def get_public_id_prefix(instance, *args, **kwargs):
+    title = instance.title
+    if title:
+        slug = slugify(title)
+        unique_id = str(uuid.uuid4()).replace("-", "")[:5]
+        return f"courses/{slug}-{unique_id}"
+    if instance.id:
+        return f"courses/{instance.id}"
+    return "courses"
+
+def get_display_name(instance, *args, **kwargs):
+    title = instance.title
+    if title:
+        return title
+    return "Course Upload"
+
 class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     # image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
-    image = CloudinaryField('image', blank=True, null=True)
+    image = CloudinaryField(
+        'image', 
+        blank=True, 
+        null=True, 
+        public_id_prefix=get_public_id_prefix, display_name=get_display_name,
+        tags=["course", "thumbnail"]
+    )
     access = models.CharField(
         max_length=5, 
         choices=AccessRequirement.choices,
@@ -31,6 +55,8 @@ class Course(models.Model):
         choices=PublishStatus.choices,
         default=PublishStatus.DRAFT
     )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     
     @property
     def is_published(self):
@@ -75,9 +101,15 @@ class Lesson(models.Model):
     description = models.TextField(blank=True, null=True)
     thumbnail = CloudinaryField('image', blank=True, null=True)
     video = CloudinaryField('video', blank=True, null=True, resource_type="video")
+    order = models.IntegerField(default=0)
     can_preview = models.BooleanField(default=False, help_text="If the user does not have access to the course, they can preview this lesson.")
     status = models.CharField(
         max_length=10, 
         choices=PublishStatus.choices,
         default=PublishStatus.PUBLISHED
     )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['order', '-updated']
